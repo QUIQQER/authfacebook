1249f89b0034be867ba22a3e4b691febf1e70134
Jan Wennrich <jan.wennrich@pcsg.de>
2020-04-20 15:44:03 +0200

    feat: adds cookie-classes for quiqqer/gdpr
    related: quiqqer/authfacebook#5

---
+++ src/QUI/Auth/Facebook/Cookies/Fbp.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @author PCSG (Jan Wennrich)
+ */
+
+namespace QUI\Auth\Facebook\Cookies;
+
+use QUI;
+use QUI\GDPR\CookieInterface;
+
+/**
+ * Class QuiqqerSessionCookie
+ *
+ * @package QUI\GDPR\Cookies
+ */
+class Fbp implements CookieInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string
+    {
+        return '_fbp';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOrigin(): string
+    {
+        return '.facebook.com';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPurpose(): string
+    {
+        return QUI::getLocale()->get('quiqqer/authfacebook', 'cookie.fbp.purpose');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getLifetime(): string
+    {
+        return \sprintf(
+            '%d %s',
+            3,
+            QUI::getLocale()->get('quiqqer/quiqqer', 'months')
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCategory(): string
+    {
+        return static::COOKIE_CATEGORY_MARKETING;
+    }
+}
